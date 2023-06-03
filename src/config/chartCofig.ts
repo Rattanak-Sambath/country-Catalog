98362ca6e10eccbed40257d5a35d900d80e6a5e4
Sambath_Rattanak <@sambathrattanak3>
2023-06-03 16:44:57 +0700

    imporve

---
+++ src/config/chartCofig.ts
@@ -0,0 +1,7 @@
+export const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      intersect: false
+    }
+  }
--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="">
-    <q-card>
-      <q-btn @click="OnClick()" />
-    </q-card>
-  </div>
-  {{ $store.state.auth.branchId }}
+    <div class="column">
+      <div class="row q-col-gutter-x-xl q-col-gutter-y-lg  ">
+            <!-- left -->
+            <div class="col-xs-12 col-md-8 col-lg-8 q-mx-auto ">
+                 <div class="row q-col-gutter-y-sm q-mx-auto">               
+                  <div class="col-12">
+                    <Bar style="height: 500px; " :data="data" :options="options" />
+
+                  </div>
+                </div>
+            </div>
+            <!-- right -->
+            <div class="col-xs-12 col-md-4 col-lg-4 q-mx-auto ">            
+             <div class="row q-my-auto q-mx-auto ">
+                <div class="col-12 ">
+                  <Pie :data="data" :options="options" />
+
+                </div>        
+            </div>
+            </div>
+      </div>
+      <div class="row q-col-gutter-x-xl q-col-gutter-y-lg  ">
+        <LineWithLineChart  :data="data" :options="options" />
+
+      </div>
+
+    </div>
+   
+  
 </template>
 
 <script setup>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,  
+  BarElement
+} from 'chart.js'
+import LineWithLineChart from '../components/LineWithLineChart.ts'
 import Toast from '../Helper/toast'
-
-const OnClick = () => {
-  Toast.info({})
+import { Pie ,Bar, } from 'vue-chartjs';
+import {options} from '../config/chartCofig.ts';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  BarElement,
+   Tooltip
+)
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
 }
+// const OnClick = () => {
+//   Toast.info({})
+// }
 </script>
 
 <style scoped></style>
